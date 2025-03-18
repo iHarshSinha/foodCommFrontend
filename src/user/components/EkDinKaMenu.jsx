1835424e1dd8fc4e10e3936dd13f695cf1e86cb5
Kartikeya Dimri <Kartikeya.Dimri@iiitb.ac.in>
2025-03-18 21:05:39 +0530

    fetch from import env

--- src/user/components/EkDinKaMenu.jsx
+++ src/user/components/EkDinKaMenu.jsx
@@ -19,7 +19,8 @@ const EkDinKaMenu = ({ day, date, isHome }) => {
   const fetchMenu = async () => {
     try {
       // const response = await fetch(`/api/user/menu`);
-      const response = await fetch(`https://foodcommbackend-production.up.railway.app/user/menu`);
+    
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/user/menu`);
       console.log("FETCHING DATA FROM :\n");
       console.log(`/api/user/menu`);
       console.log("harsh added this line");
